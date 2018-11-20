7210df41b35af95f815baf8a80e8baa6a9e5f1a3
Prateek_Tewari <hindustani1000@gmail.com>
2018-11-20 20:28:05 +0530

    Added alt text for images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -97,6 +97,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById("restaurant-img");
   image.className = "restaurant-img";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `The awesome ${restaurant.name} Restaurant`;
 
   const cuisine = document.getElementById("restaurant-cuisine");
   cuisine.innerHTML = restaurant.cuisine_type;
